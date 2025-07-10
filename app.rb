7d202434d82bba05aa83d864478d15341be8b7a4
Kovalenko1984 <pope2007@ukr.net>
2025-07-10 11:14:56 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,16 +2,15 @@ require 'sinatra'
 require 'json'
 require 'rack/protection'
 
-# Дозволити лише домени Render і локальні (уникаємо помилок 403)
-use Rack::Protection::HostAuthorization, hosts: lambda { |host| 
-  host.end_with?('.onrender.com') || host == 'localhost' 
-}
+# Дозволяємо Render-домен
+use Rack::Protection::HostAuthorization, hosts: [
+  'orthocal-uk.onrender.com',
+  /\.onrender\.com$/
+]
 
-# Параметри запуску
 set :bind, '0.0.0.0'
 set :port, 3000
 
-# Тестовий ендпоінт
 get '/' do
   content_type :json
   {
@@ -20,19 +19,15 @@ get '/' do
   }.to_json
 end
 
-# Основний API
 get '/api/calendar' do
   content_type :json
 
-  # Отримати параметри
   month = params['month']&.to_i || Time.now.month
   year = params['year']&.to_i || Time.now.year
 
-  # Тимчасово: згенерувати простий календар
   {
     year: year,
     month: month,
     days: (1..31).map { |d| { day: d, feast: "Свято №#{d}" } }
   }.to_json
 end
-
